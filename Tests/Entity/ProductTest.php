95aa238dd8a7ea1ae9b99067edf81edfbadf0e48
GasanovShamil <gasanov.sh@gmail.com>
2018-02-16 11:06:23 +0100

    tests unitaires

---
+++ Tests/Entity/ProductTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Entity;
+
+use Entity\Product;
+use PHPUnit\Framework\TestCase;
+
+class ProductTest extends TestCase
+{
+    public function testcomputeTVAFoodProduct()
+    {
+        $product = new Product('Un produit', Product::FOOD_PRODUCT, 20);
+
+        $this->assertSame(1.1, $product->computeTVA());
+    }
+}
